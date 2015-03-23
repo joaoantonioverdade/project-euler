8cada9f94b397090cc283f0a038ef2180407b5b8
joaoantonioverdade <joao.antonio.verdade@gmail.com>
2015-03-23 23:02:42 +0000

    Problem 5 work in progress II...

--- id5.py
+++ id5.py
@@ -18,17 +18,26 @@ def no_remainder(total_divisible):
 	n = 1
 	while True:
 
-		for i in range(1,total_divisible + 1):
+		for i in total_divisible: #range(1,total_divisible + 1):
 
 			if(n % i) != 0:
 				break
 			
-			if i == total_divisible:
+			if i == len(total_divisible):
 				return n
 
 		n += 1
 
 if __name__ == "__main__":
-	print(no_remainder(20))
+	print(no_remainder([20,19,18,17,16,15,14,13,12,11]))
 
-	
+#20 10 5
+#19 8 4 2 1
+#18 9 3 
+#17
+#16 8
+#15 5
+#14 7
+#13
+#12 6 3 
+#11
