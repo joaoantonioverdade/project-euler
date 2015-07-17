4090b080acc701cbe646dca21a102093e19ba657
joaoantonioverdade <joao.antonio.verdade@gmail.com>
2015-07-17 15:39:21 +0100

    Solution to problem 67

--- id67.py
+++ id67.py
@@ -31,48 +31,12 @@ if __name__ == "__main__":
     numbers = []
     for line in read_data.split("\n"):
         if line is not "":
-            numbers.append(line)
+            numbers.append([int(n) for n in line.split(" ")])
 
-    # first value
-    ref_count = int(numbers[0])
-    ref_id = 0
-    numbers.pop(0)
+    while(len(numbers) > 1):
 
-    for idx, line in enumerate(numbers):
+        first_line = numbers.pop()
+        for idx, n in enumerate(numbers[-1]):
+            numbers[-1][idx] += max(first_line[idx:idx+2])
 
-        all_actual_options = [int(num) for num in numbers[idx].split(" ")]
-        actual_options = all_actual_options[ref_id:ref_id + 2]
-
-        # find max from last line
-        if len(numbers) <= idx + 1:
-            ref_count += max(actual_options)
-            break
-
-        all_next_options = [int(num) for num in numbers[idx + 1].split(" ")]
-
-        next_options = all_next_options[ref_id:]
-
-        # print()
-        # print("Actual Options", actual_options)
-        # print("Next Options", next_options[:4])
-
-        max_option = 0
-        ref_temp = 0
-        for idx_option, option in enumerate(actual_options):
-
-            left_num = next_options[idx_option]
-            right_num = next_options[idx_option + 1]
-
-            if option + right_num > max_option:
-                max_option = option + right_num
-                ref_temp = idx_option
-
-            if option + left_num > max_option:
-                max_option = option + left_num
-                ref_temp = idx_option
-
-        ref_id += ref_temp
-        ref_count += all_actual_options[ref_id]
-
-        print("Ref", all_actual_options[ref_id], ref_count)
-    print("Sum", ref_count)
+    print(numbers)
